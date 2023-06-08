1854ea6ce1ebf16fe12059d7da0719b1a74151e5
Sotiris Emmanouil <108318635+SotirisEmmanouil@users.noreply.github.com>
2023-06-08 13:32:08 -0400

    Update CustomComparator.java

--- CS340 Project 1/src/CustomComparator.java
+++ CS340 Project 1/src/CustomComparator.java
@@ -2,7 +2,7 @@ import java.util.Comparator;
 
 public class CustomComparator implements Comparator<PS> {
 	   
-		@Override
+	    @Override
 	    public int compare(PS o1, PS o2) {
 			
 			if(o1.getArr() == o2.getArr()) {	//if the arrival times are the same, compare the priorities 
